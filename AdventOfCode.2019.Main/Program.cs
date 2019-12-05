8a5f3bba0cef2eea2553ea090362eed288f63408
Ioana Coruga <Ioana.Coruga@igentics.com>
2019-12-05 19:38:51 +0000

    Add new folder structure

--- AdventOfCode.2019.Main/Program.cs
+++ AdventOfCode.2019.Main/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.IO;
+using AdventOfCode.Core;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
 
 namespace AdventOfCode2019.Main
 {
@@ -6,7 +10,16 @@ namespace AdventOfCode2019.Main
     {
         static void Main(string[] args)
         {
+            IFileProvider physicalProvider = new PhysicalFileProvider(Directory.GetCurrentDirectory());
 
+            //setup our DI
+            var serviceProvider = new ServiceCollection()
+                .AddSingleton<IFileProvider>(physicalProvider)
+                .BuildServiceProvider();
+
+            //do the actual work here
+            var fileImporter = serviceProvider.GetService<IFileImporter>();
+            var frequencies = fileImporter.ReadFile("AdventOfCode2019_Day1.txt");
 
             Console.WriteLine("Hello World!");
         }
---
+++ AdventOfCode.Core/FileImporter.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using Microsoft.Extensions.FileProviders;
+
+namespace AdventOfCode.Core
+{
+    public class FileImporter : IFileImporter
+    {
+        private readonly IFileProvider _fileProvider;
+
+        public FileImporter(IFileProvider fileProvider)
+        {
+            _fileProvider = fileProvider;
+        }
+
+        public IEnumerable<int> ReadFile(string path)
+        {
+            var list = new List<int>();
+
+            using (var stream = _fileProvider.GetFileInfo(path).CreateReadStream())
+            {
+                using (var reader = new StreamReader(stream))
+                {
+                    while (!reader.EndOfStream)
+                    {
+                        var line = reader.ReadLine();
+                        list.Add(int.Parse(line));
+                    }
+                }
+            };
+
+            return list;
+        }
+    }
+}
